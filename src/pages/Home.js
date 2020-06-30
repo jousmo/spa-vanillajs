6d207aba013e7969d7d72b5defd14cd8d3a55776
Jose Uscanga <jousmo@outlook.com>
2020-06-30 13:16:02 -0500

    Create pages and templates base

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Home = () => {
+  const view = `
+    <div class='Characters'>
+      <article class='Character__item'>
+        <a href='#/1'>
+          <img src='image' alt='name'>
+          <h2>Name</h2>
+        </a>
+      </article>
+    <div>
+  `
+
+  return view
+}
+
+export default Home
---
+++ src/templates/Header.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Header = () => {
+  const view = `
+    <div class='Header-main'>
+      <div class='Header-logo'>
+        <h1>
+          <a href='/'>
+            Spa App Example
+          </a>
+        </h1>
+      </div>
+      <div class='Header-nav'>
+        <a href='#/about/'>
+          About
+        </a>
+      </div>
+    <div>
+  `
+
+  return view
+}
+
+export default Header
